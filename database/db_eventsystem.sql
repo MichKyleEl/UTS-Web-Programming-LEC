c4830116d1f419bab37f9e99983fc6e115457963
David G Tech <76923507+davidgrcias@users.noreply.github.com>
2024-10-06 03:42:09 +0700

    perbaikin database

--- database/db_eventsystem.sql
+++ database/db_eventsystem.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 05, 2024 at 05:26 PM
+-- Generation Time: Oct 05, 2024 at 10:40 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -24,19 +24,6 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tb_admin`
---
-
-CREATE TABLE `tb_admin` (
-  `admin_id` int(11) NOT NULL,
-  `admin_name` varchar(50) NOT NULL,
-  `admin_email` varchar(50) NOT NULL,
-  `admin_password` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `tb_event`
 --
 
@@ -53,6 +40,14 @@ CREATE TABLE `tb_event` (
   `event_status` enum('open','closed','canceled') DEFAULT 'open'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `tb_event`
+--
+
+INSERT INTO `tb_event` (`event_id`, `event_name`, `event_description`, `event_date`, `event_time`, `event_location`, `max_participants`, `event_image`, `event_banner`, `event_status`) VALUES
+(1, 'AYCE Wingstop', 'Makan sepuasnya 2 jam', '27 September 2024', '13:00', 'Jakarta', 50, 'fotomakan.jpg', 'ayce.jpg', 'open'),
+(2, 'HUT TNI', 'Bakal bikin macet seJakarta', '05 Oktober 2024', '08:00', 'Monumen Nasional', 2000, 'tnijaya.jpg', 'tni.png', 'closed');
+
 -- --------------------------------------------------------
 
 --
@@ -66,6 +61,15 @@ CREATE TABLE `tb_registration` (
   `registration_date` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `tb_registration`
+--
+
+INSERT INTO `tb_registration` (`registration_id`, `user_id`, `event_id`, `registration_date`) VALUES
+(1, 2, 1, '2024-10-05 19:50:31'),
+(2, 3, 2, '2024-10-05 19:56:14'),
+(3, 4, 2, '2024-10-05 19:56:22');
+
 -- --------------------------------------------------------
 
 --
@@ -74,21 +78,26 @@ CREATE TABLE `tb_registration` (
 
 CREATE TABLE `tb_user` (
   `user_id` int(11) NOT NULL,
-  `user_name` varchar(10) NOT NULL,
-  `user_email` varchar(50) NOT NULL,
-  `user_password` varchar(255) NOT NULL
+  `user_name` varchar(100) NOT NULL,
+  `user_email` varchar(100) NOT NULL,
+  `user_password` varchar(255) NOT NULL,
+  `role` enum('admin','user') NOT NULL DEFAULT 'user',
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tb_user`
+--
+
+INSERT INTO `tb_user` (`user_id`, `user_name`, `user_email`, `user_password`, `role`, `created_at`) VALUES
+(1, 'Admin', 'admin@gmail.com', 'admingrup5thebest', 'admin', '2024-10-05 18:01:28'),
+(2, 'David Garcia Saragih', 'david@gmail.com', '$2y$10$ECcXQNVz5t9qfH2uaDwDD.Wp8GXOeRT1RVADeplYyIUP7uzGdL/mK', 'user', '2024-10-05 18:43:13'),
+(3, 'Michael Elbert', 'michael@gmail.com', '$2y$10$FwOzxf9B0CGr09tQCo9C5eA11nkzoYjWYFBGRU5pZtQOOo/8fYIyO', 'user', '2024-10-05 19:09:08'),
+(4, 'Ben Arief Sihotang', 'benarief@gmail.com', '$2y$10$sX49QKcCQPBPw3pbIKFuk.IgdFoDFuz8pfaPRGvskOf7t03XkLdCu', 'user', '2024-10-05 19:09:23');
 
 --
 -- Indexes for dumped tables
 --
-
---
--- Indexes for table `tb_admin`
---
-ALTER TABLE `tb_admin`
-  ADD PRIMARY KEY (`admin_id`),
-  ADD UNIQUE KEY `admin_email` (`admin_email`);
 
 --
 -- Indexes for table `tb_event`
@@ -108,35 +117,30 @@ ALTER TABLE `tb_registration`
 -- Indexes for table `tb_user`
 --
 ALTER TABLE `tb_user`
-  ADD PRIMARY KEY (`user_id`);
+  ADD PRIMARY KEY (`user_id`),
+  ADD UNIQUE KEY `user_email` (`user_email`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `tb_admin`
---
-ALTER TABLE `tb_admin`
-  MODIFY `admin_id` int(11) NOT NULL AUTO_INCREMENT;
-
---
 -- AUTO_INCREMENT for table `tb_event`
 --
 ALTER TABLE `tb_event`
-  MODIFY `event_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `event_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `tb_registration`
 --
 ALTER TABLE `tb_registration`
-  MODIFY `registration_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `registration_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `tb_user`
 --
 ALTER TABLE `tb_user`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Constraints for dumped tables
