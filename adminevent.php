76cb086d9189c5f92115228859cb370da8ebd7d6
BenArief <benarief29@gmail.com>
2024-10-23 14:50:58 +0700

    nambahin tabel buat export csv,dkk

--- adminevent.php
+++ adminevent.php
@@ -31,44 +31,46 @@ $tabelevent = query("SELECT * FROM tb_event");
     <?php
     require 'features/pagetitle.php';
     ?>
-    List of existing events
-    <div class="table-responsive">
-        <table class="table table-striped table-hover datatable">
-            <thead>
-                <tr>
-                    <th>Action</th>
-                    <th>Event Name</th>
-                    <th>Description</th>
-                    <th>Date</th>
-                    <th>Time</th>
-                    <th>Location</th>
-                    <th>Max Participant</th>
-                    <th>image</th>
-                    <th>banner</th>
-                    <th>event_status</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php $i = 1; ?>
-                <?php foreach ($tabelevent as $row) : ?>
+    <div class="card mb-3">
+        List of existing events
+        <div class="table-responsive">
+            <table class="table table-striped table-hover display" id="table6">
+                <thead>
                     <tr>
-                        <td><a href="delete.php?event_id=<?= $row["event_id"]; ?>" onclick="return confirm('Are you sure want to delete this data?');" class="btn btn-danger btn-sm">Delete</a>
-                            <a href="editevent.php?event_id=<?= $row["event_id"]; ?>" class="btn btn-warning mt-1">Edit</a>
-                        </td>
-                        <td><?= $row["event_name"]; ?></td>
-                        <td><?= $row["event_description"]; ?></td>
-                        <td><?= $row["event_date"]; ?></td>
-                        <td><?= $row["event_time"]; ?></td>
-                        <td><?= $row["event_location"]; ?></td>
-                        <td><?= $row["max_participants"]; ?></td>
-                        <td><img src="uploads/<?= $row["event_image"]; ?>" width="90"></td>
-                        <td><img src="uploads/<?= $row["event_banner"]; ?>" width="90"></td>
-                        <td><?= $row["event_status"]; ?></td>
+                        <th>Action</th>
+                        <th>Event Name</th>
+                        <th>Description</th>
+                        <th>Date</th>
+                        <th>Time</th>
+                        <th>Location</th>
+                        <th>Max Participant</th>
+                        <th>image</th>
+                        <th>banner</th>
+                        <th>event_status</th>
                     </tr>
-                    <?php $i++; ?>
-                <?php endforeach; ?>
-            </tbody>
-        </table>
+                </thead>
+                <tbody>
+                    <?php $i = 1; ?>
+                    <?php foreach ($tabelevent as $row) : ?>
+                        <tr>
+                            <td><a href="delete.php?event_id=<?= $row["event_id"]; ?>" onclick="return confirm('Are you sure want to delete this data?');" class="btn btn-danger btn-sm">Delete</a>
+                                <a href="editevent.php?event_id=<?= $row["event_id"]; ?>" class="btn btn-warning mt-1">Edit</a>
+                            </td>
+                            <td><?= $row["event_name"]; ?></td>
+                            <td><?= $row["event_description"]; ?></td>
+                            <td><?= $row["event_date"]; ?></td>
+                            <td><?= $row["event_time"]; ?></td>
+                            <td><?= $row["event_location"]; ?></td>
+                            <td><?= $row["max_participants"]; ?></td>
+                            <td><img src="uploads/<?= $row["event_image"]; ?>" width="90"></td>
+                            <td><img src="uploads/<?= $row["event_banner"]; ?>" width="90"></td>
+                            <td><?= $row["event_status"]; ?></td>
+                        </tr>
+                        <?php $i++; ?>
+                    <?php endforeach; ?>
+                </tbody>
+            </table>
+        </div>
     </div>
 
     <div class="d-flex justify-content-end">
